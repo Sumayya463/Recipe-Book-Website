89b9401cf5115f815b9404972ce05bf5c873f248
Sumayya463 <sumayyajaveed463@gmail.com>
2025-01-25 00:26:25 +0530

    Add files via upload

---
+++ homePage.js
@@ -0,0 +1,191 @@
+
+        const breakfast = document.getElementById('breakfast');
+        breakfast.addEventListener('click', () => {
+        window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/breakfast.html", "_self");
+        });
+        
+        const lunch = document.getElementById('lunch');
+        lunch.addEventListener('click', () => {
+        window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/lunch.html", "_self");
+        });
+        
+        
+        const dinner = document.getElementById('dinner');
+        dinner.addEventListener('click', () => {
+        window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/dinner.html", "_self");
+        });
+        
+        const searching = document.getElementById("recipe-input");
+        
+        const search = document.getElementById("search-recipe");
+        const errorContainer = document.getElementById('error-message');
+        search.addEventListener('click', ()=>{
+        const inputValue = searching.value;
+        
+        if(inputValue === "pancakes"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=1")
+        }
+        if(inputValue === "shakshuka"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=2")
+        }
+        if(inputValue === "chole bhature"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=3")
+        }
+        if(inputValue === "croissant "){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=4")
+        }
+        if(inputValue === "scramcled eggs"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=5")
+        }
+        if(inputValue === "rawa upma"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=6")
+        }
+        if(inputValue === "spanish omelette"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=7")
+        }
+        if(inputValue === "simit"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=8")
+        }
+        if(inputValue === " bao buns"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=9")
+        }
+        if(inputValue === " chicken porridge"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=10")
+        }
+        if(inputValue === "dosa"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=11")
+        }
+        if(inputValue === "fara"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=12")
+        }
+        if(inputValue === "paya nihari"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=13")
+        }
+        if(inputValue === "french toast"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=14")
+        }
+        if(inputValue === "oatmeal"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=15")
+        }
+        if(inputValue === "idli"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=16")
+        }
+        if(inputValue === "rajma chawal"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=17")
+        }
+        if(inputValue === "kimbap"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=18")
+        }
+        if(inputValue === "frittata"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=19")
+        }
+        if(inputValue === "falafel"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=20")
+        }
+        if(inputValue === "club sandwich"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=21")
+        }
+        if(inputValue === "chickpea salad"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=22")
+        }
+        if(inputValue === "tuna wrap"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=23")
+        }
+        if(inputValue === "dal makhani"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=24")
+        }
+        if(inputValue === "kathi roll"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=25")
+        }
+        if(inputValue === "tomato soup"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=26")
+        }
+        if(inputValue === "tacos"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=27")
+        }
+        if(inputValue === "dal tadka"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=28")
+        }
+        if(inputValue === "fajitas"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=29")
+        }
+        if(inputValue === "chicken soup"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=30")
+        }
+        if(inputValue === "egg masala "){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=31")
+        }
+        if(inputValue === "paneer fried rice"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=32")
+        }
+        if(inputValue === "chicken biryani"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=33")
+        }
+        if(inputValue === "chicken kebab"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=34")
+        }
+        if(inputValue === "lazagna"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=35")
+        }
+        if(inputValue === "garlic shrimp"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=36")
+        }
+        if(inputValue === "haleem"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=37")
+        }
+        if(inputValue === "chicken pizza"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=38")
+        }
+        if(inputValue === "alfredo pasta"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=39")
+        }
+        if(inputValue === "crispy fried chicken"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=40")
+        }
+        if(inputValue === "veg noodles"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=41")
+        }
+        if(inputValue === "cream of chciken soup "){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=42")
+        }
+        if(inputValue === "butter chicken"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=43")
+        }
+        if(inputValue === "paneer butter masala"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=44")
+        }
+        if(inputValue === "mutton rogan josh"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=45")
+        }
+        if(inputValue === "aloo gobi"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=46")
+        }
+        if(inputValue === "veg pulao"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=47")
+        }
+        if(inputValue === "sarson ka saag"){
+            window.open("file:///C:/Users/Anas%20Ali/Desktop/My%20Projects/Recipe%20Book%20Website/recipe.html?id=48")
+        }
+        if(inputValue === ''){
+            alert('Search For a recipe..!')
+        }
+        else{
+           
+            errorContainer.textContent = 'Recipe not found. Please try again.';
+            errorContainer.style.color = 'white';
+            errorContainer.style.backgroundColor = 'black';
+        }   
+       
+       
+        });
+
+        searching.addEventListener('input', function () {
+            errorContainer.textContent = ''; // Clear the error message
+        });
+       
+
+   
+    
+
+
+
